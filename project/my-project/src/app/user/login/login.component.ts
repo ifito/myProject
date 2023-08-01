f23143940737365ab44c6b015559bff31713c544
Ivelina Bakardzhieva <georgievaa23@abv.bg>
2023-08-01 12:42:34 +0300

    Update login.component.ts

--- project/my-project/src/app/user/login/login.component.ts
+++ project/my-project/src/app/user/login/login.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(private userService: UserService) {
 
+  }
+
+  login(email: string, password: string): void {
+    
+
+    this.userService.login();
+  }
 }
